36b02b7a5852c0c82de6928025897dbdba2736c1
FrankZhaoYX <zhaoyixi0306@gmail.com>
2025-08-24 11:15:38 -0400

    relase the zip file, modify the pom file to fit my local env

---
+++ frank_BrainRidge_interview/src/main/java/com/bank/frank_BrainRidge_interview/FrankBrainRidgeInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.bank.frank_BrainRidge_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankBrainRidgeInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrankBrainRidgeInterviewApplication.class, args);
+	}
+
+}
---
+++ frank_BrainRidge_interview/src/test/java/com/bank/frank_BrainRidge_interview/FrankBrainRidgeInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.frank_BrainRidge_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankBrainRidgeInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
